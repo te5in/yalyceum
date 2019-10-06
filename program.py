6bc43e0eb3f6b0b3face5a8066a510f276d19a7b
te5in <tesin@fmschool72.ru>
2019-10-06 13:27:35 +0500

    2. Флаг РФ reformat

--- program.py
+++ program.py
@@ -4,23 +4,22 @@ pygame.init()
 
 size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
-white = pygame.Color('white')
 
 def draw():
-    pygame.draw.line(screen, white, (50, 450), (50, 100))
+    pygame.draw.line(screen, pygame.Color('white'), (50, 450), (50, 100))
     red()
     blue()
     white()
 
-
-
 def red():
     pygame.draw.rect(screen, pygame.Color('red'), ((50, 220), (350, 60)))
+
 def blue():
     pygame.draw.rect(screen, pygame.Color('blue'), ((50, 160), (350, 60)))
+
 def white():
     rectPoint = ((51, 100), (350, 60))
-    pygame.draw.rect(screen, white, rectPoint)
+    pygame.draw.rect(screen, pygame.Color('white'), rectPoint)
 
 draw()
 
